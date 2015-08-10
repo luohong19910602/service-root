d83c303eb707d440a6fa8d5b381be431839c47fa
luohong <846705189@qq.com>
2015-08-10 21:34:09 +0800

    初始化代码

---
+++ email/src/main/java/com/skg/luohong/service/email/App.java
@@ -0,0 +1,13 @@
+package com.skg.luohong.service.email;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ solr/src/main/java/com/skg/luohong/service/solr/App.java
@@ -0,0 +1,13 @@
+package com.skg.luohong.service.solr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
